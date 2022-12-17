1efc4a2d7faaec6536f4ac04a25ddbdaad780a7d
Meng Thao <mengthao023@gmail.com>
2022-12-17 14:33:24 -0600

    improved rendering list (complete-rending-lisit)

--- frontend/src/Data.js
+++ frontend/src/Data.js
@@ -1,4 +1,16 @@
 import SilverTempest from "./img/Silver Tempest.jpg"
+import LostOrigin from "./img/Lost Origin.jpg"
+import PokemonGo from "./img/Pokemon Go.jpg"
+import AstralRadiance from "./img/Astral Radiance.jpg"
+import BrilliantStar from "./img/Brilliant Stars.jpg"
+import FusionStrike from "./img/Fusion Strike.jpg"
+import Celebration from "./img/Celebration.jpg"
+import EvolvingSkies from "./img/Evolving Skies.jpg"
+import ChillingReign from "./img/Chilling Reign.jpg"
+import BattleStyle from "./img/Battle Style.jpg"
+import ShiningFate from "./img/Shinning Fate.jpg"
+import VividVoltage from "./img/Vivid Voltage.jpg"
+
 
 export const Data = [
     {
@@ -11,24 +23,66 @@ export const Data = [
         id: "2",
         title: "Lost Origin",
         name: "Pokemon",
-        image: SilverTempest
+        image: LostOrigin
     },
     {
         id: "3",
         title: "Pokemon Go",
         name: "Pokemon",
-        image: SilverTempest
+        image: PokemonGo
     },
     {
         id: "4",
         title: "Astral Radiance",
         name: "Pokemon",
-        image: SilverTempest
+        image: AstralRadiance
     },
     {
         id: "5",
         title: "Brilliant Stars",
         name: "Pokemon",
-        image: SilverTempest
-    }
+        image: BrilliantStar
+    },
+    {
+        id: "6",
+        title: "Fusion Strike",
+        name: "Pokemon",
+        image: FusionStrike
+    },
+    {
+        id: "7",
+        title: "Celebration",
+        name: "Pokemon",
+        image: Celebration
+    },
+    {
+        id: "8",
+        title: "Evolving Skies",
+        name: "Pokemon",
+        image: EvolvingSkies
+    },
+    {
+        id: "9",
+        title: "Chilling Reign",
+        name: "Pokemon",
+        image: ChillingReign
+    },
+    {
+        id: "10",
+        title: "",
+        name: "Battle Styles",
+        image: BattleStyle
+    },
+    {
+        id: "11",
+        title: "Shining Fates",
+        name: "Pokemon",
+        image: ShiningFate
+    },
+    {
+        id: "12",
+        title: "Vivid Voltage",
+        name: "Pokemon",
+        image: VividVoltage
+    },
 ]
