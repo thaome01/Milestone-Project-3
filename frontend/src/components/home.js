60df8dc9e374753c0593e86a5a7c82c14211f24c
Meng Thao <mengthao023@gmail.com>
2022-12-18 09:32:45 -0600

    improved grid (adjustment-grid)

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -4,18 +4,18 @@ import '../style/posting.css'
 function home() {
   return (
     <div className="container">
+    <div className="cards"><img src="/img/Astral Radiance.jpg" alt="pokemon" /></div>
     <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
-    <div className="cards"><img src="/img/Battle Style.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Brilliant Stars.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Celebration.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Chilling Reign.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Evolving Skies.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Fusion Strike.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Lost Origin.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Pokemon Go.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Shinning Fate.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Silver Tempest.jpg" alt="pokemon" /></div>
+    <div className="cards"><img src="/img/Vivid Voltage.jpg" alt="pokemon" /></div>
     </div>
   
   )
